6bb0389f5bc4175eca5cf263d613b700798cc0b1
Richard Lupton <richard.lupton@gmail.com>
2017-03-16 23:46:10 +0000

    Basic warp application.

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Protolude
 import Lib
 
+import Network.Wai (Application, responseLBS)
+import Network.HTTP.Types (status200)
+import Network.Wai.Handler.Warp (run)
+
 main :: IO ()
-main = someFunc
+main = print "Klashnikov" >> run 8080 balancer
+
+balancer :: Application
+balancer _ respond = respond $ responseLBS status200
+                     [("Content-Type", "text/plain")]
+                     "Test"
