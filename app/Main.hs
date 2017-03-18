8ab238e6c651a86671518cafe8c594d1c27d7943
Richard Lupton <richard.lupton@gmail.com>
2017-03-18 19:53:27 +0000

    Basic hardcoded reverse proxy
    Found a reverse proxying library and wrote a basic reverse proxy.

--- app/Main.hs
+++ app/Main.hs
@@ -4,14 +4,18 @@ module Main where
 import Protolude
 import Lib
 
-import Network.Wai (Application, responseLBS)
-import Network.HTTP.Types (status200)
+import Network.Wai (Application)
 import Network.Wai.Handler.Warp (run)
 
-main :: IO ()
-main = print "Klashnikov" >> run 8080 balancer
+import Network.HTTP.ReverseProxy (ProxyDest(..), WaiProxyResponse(..)
+                                 , waiProxyTo, defaultOnExc)
+import Network.HTTP.Client (newManager, defaultManagerSettings)
 
-balancer :: Application
-balancer _ respond = respond $ responseLBS status200
-                     [("Content-Type", "text/plain")]
-                     "Test"
+main :: IO ()
+main = do
+  manager <- newManager defaultManagerSettings
+  run 8080 $ waiProxyTo (\_ -> return $ WPRProxyDest proxy) defaultOnExc manager
+
+
+proxy :: ProxyDest
+proxy = ProxyDest "localhost" 8000
