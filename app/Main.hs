cf2d79e7c6eeaa2bafbe9c83badfa85259ff4e17
Richard Lupton <richard.lupton@gmail.com>
2017-04-04 23:23:47 +0100

    Striaghtforward tidying

--- app/Main.hs
+++ app/Main.hs
@@ -4,32 +4,32 @@
 {-# LANGUAGE BangPatterns #-}
 module Main where
 
-import Protolude
-import Lib
+import           Protolude
 
-import Network.Wai (Request, responseLBS)
-import Network.Wai.Handler.Warp (run)
+import           Network.Wai (Request, responseLBS)
+import           Network.Wai.Handler.Warp (run)
 
-import Network.HTTP.ReverseProxy (ProxyDest(..), WaiProxyResponse(..)
-                                 , waiProxyTo, defaultOnExc)
-import Network.HTTP.Client (newManager, defaultManagerSettings)
-import Network.HTTP.Types (serviceUnavailable503)
-import Data.IORef (IORef, newIORef, readIORef, modifyIORef')
-import Control.Concurrent.STM.TVar (TVar, newTVarIO, writeTVar, readTVar)
-import Data.Text (Text)
-import qualified Data.Text.Lazy.Encoding as EL
-import qualified Data.Text.Encoding as E
-import qualified Data.Vector as V
-import Data.Vector ((!?))
-import Data.Text.Lazy (fromStrict)
-import Data.String (String)
+import           Control.Concurrent.STM.TVar ( TVar, newTVarIO
+                                             , writeTVar, readTVar )
+import           Data.Aeson ((.:))
 import qualified Data.Aeson as A
-import Data.Aeson ((.:))
-import Data.Tyro (type( >%> ), List, Extract, unwrap)
+import           Data.IORef (IORef, newIORef, readIORef, modifyIORef')
+import           Data.String (String)
+import           Data.Text (Text)
+import qualified Data.Text.Encoding as E
+import           Data.Text.Lazy (fromStrict)
+import qualified Data.Text.Lazy.Encoding as EL
+import           Data.Tyro (type( >%> ), List, Extract, unwrap)
+import           Data.Vector ((!?))
+import qualified Data.Vector as V
+import           Network.HTTP.Client (newManager, defaultManagerSettings)
+import           Network.HTTP.ReverseProxy (ProxyDest(..), WaiProxyResponse(..)
+                                           , waiProxyTo, defaultOnExc)
+import           Network.HTTP.Types (serviceUnavailable503)
 
-import Control.Monad.State (StateT, evalStateT)
-import Control.Concurrent.Stack (Stack, register, runStack)
-import Database.Etcd (Etcd, EtcdM, etcd, runEtcd, watchDirectoryFromIndex, getDirectory)
+import           Control.Concurrent.Stack (Stack, register, runStack)
+import           Database.Etcd ( Etcd, EtcdM, etcd, runEtcd
+                               , watchDirectoryFromIndex, getDirectory )
 
 
 
@@ -37,9 +37,9 @@ import Database.Etcd (Etcd, EtcdM, etcd, runEtcd, watchDirectoryFromIndex, getDi
 -- Core types
 --------------------------------------------------------------------------------
 
-type Manager = ReaderT LoadBalancing (StateT Integer IO)
+type Manager = ReaderT BalancerEnvironment (StateT Integer IO)
 
-runManager :: LoadBalancing -> Integer -> Manager a -> IO a
+runManager :: BalancerEnvironment -> Integer -> Manager a -> IO a
 runManager l i m = evalStateT (runReaderT m l) i
 
 
@@ -75,34 +75,36 @@ core = do
       put (i+1)
 
 
-balancer :: LoadBalancing -> (Request -> IO WaiProxyResponse)
+balancer :: BalancerEnvironment -> (Request -> IO WaiProxyResponse)
 balancer env = \_ -> do
   v <- atomically $ readTVar (proxies env)
   let l = V.length v
   n <- readIORef (current env)
   let i = if n >= l then 0 else n
       proxy = v !? i
-  putStrLn $ "Using backend" ++ show i
   modifyIORef' (current env) (\_ -> if i + 1 == l then 0 else i + 1 )
-  return $ maybe (WPRResponse $ responseLBS serviceUnavailable503 [] "") WPRProxyDest proxy
+  let respond503 = (WPRResponse $ responseLBS serviceUnavailable503 [] "")
+  return $ maybe respond503 WPRProxyDest proxy
 
 
 --------------------------------------------------------------------------------
 -- Obtaining list of backends
 --------------------------------------------------------------------------------
 
+type ExtractNodeValues a = "node" >%> "nodes" >%> List ("value" >%> Extract a)
+
 listOfBackendsOnEvent :: String -> Integer -> EtcdM [Backend]
 listOfBackendsOnEvent path watchIndex = do
   _ <- watchDirectoryFromIndex path watchIndex :: EtcdM (Maybe ())
-  be <- getDirectory path :: EtcdM (Maybe ("node" >%> "nodes" >%> List ("value" >%> Extract Text)))
+  be <- getDirectory path :: EtcdM (Maybe (ExtractNodeValues Text))
   let maybeListToList = maybe [] identity
       parsed = maybeListToList ((fmap (fmap unwrap)) $ (fmap unwrap be))
       structure = maybeListToList . sequence . filter isJust . fmap parseBackend
   return $ structure parsed
 
 buildBalancerStructure :: [Backend] -> V.Vector ProxyDest
-buildBalancerStructure = let !encodeAddress = E.encodeUtf8 . address
-                             !toDest = ProxyDest <$> encodeAddress <*> port in
+buildBalancerStructure = let encodeAddress = E.encodeUtf8 . address
+                             toDest = ProxyDest <$> encodeAddress <*> port in
   V.fromList . map toDest
 
 
@@ -115,7 +117,8 @@ data Backend = Backend { name :: Text
                        , port :: Int } deriving (Show, Eq)
 
 instance A.FromJSON Backend where
-  parseJSON (A.Object v) = Backend <$> v .: "name" <*> v .: "address" <*> v .: "port"
+  parseJSON (A.Object v) = Backend <$>
+    v .: "name" <*> v .: "address" <*> v .: "port"
   parseJSON _ = empty
 
 parseBackend :: Text -> Maybe Backend
@@ -126,11 +129,11 @@ parseBackend = A.decode . EL.encodeUtf8 . fromStrict
 -- Load balancing environment structure
 --------------------------------------------------------------------------------
 
-data LoadBalancing = LoadBalancing { current :: IORef Int
-                                   , proxies :: TVar (V.Vector ProxyDest) }
+data BalancerEnvironment = BE { current :: IORef Int
+                              , proxies :: TVar (V.Vector ProxyDest) }
 
-newEnvironment :: IO LoadBalancing
+newEnvironment :: IO BalancerEnvironment
 newEnvironment = do
   c <- newIORef 0
   p <- newTVarIO V.empty
-  return $ LoadBalancing c p
+  return $ BE c p
